a2d657d48ceeb7fd1470486a6414f3d035e12b71
Andrew <andrew@gmail.com>
2021-01-28 11:07:01 +0300

    second try

--- logic.js
+++ logic.js
@@ -1,23 +1,34 @@
 // Получаю выбранную строку:
-drop.onclick = function() {
+drop.onchange = function() {
     inp.value = this.value;
 };
 
 // По нажатию отправляю содержание строки:
 button.onclick = function() {
 
-    let i = 0;
-    let leng = document.getElementsByTagName('option').length;
+    // let i = 0;
+    // let leng = document.querySelectorAll('option').length;
 
-    for (let i = 0; i < leng; i++) {
-        if (drop[i].value === drop.value) {
+    // for (let i = 0; i < leng; i++) {
+    //     if (drop[i].value === drop.value) {
+    //         if (inp.value !== '') { 
+    //             drop[i].innerHTML = inp.value;
+    //         } else {
+    //             document.getElementById("inp").style.borderColor = "red";
+    //         } 
+    //     }
+    // }
+    
+    let elems = document.querySelectorAll('option');
+
+    elems.forEach(element => {
+        if (element.value === drop.value) {
             if (inp.value !== '') { 
-                drop[i].innerHTML = inp.value;
+                element.innerHTML = inp.value;
             } else {
                 document.getElementById("inp").style.borderColor = "red";
-                setTimeout(() => document.getElementById("inp").style.borderColor = "", 1000);
             } 
         }
-    }
+    });
 
   };
