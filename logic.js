572ceef0a099c52530818c9a1ea5a02cd4455d30
Andrew <andrew@gmail.com>
2021-01-28 11:46:40 +0300

    third try

--- logic.js
+++ logic.js
@@ -1,34 +1,21 @@
 // Получаю выбранную строку:
-drop.onchange = function() {
+drop.onchange = function () {
     inp.value = this.value;
+    document.getElementById("inp").style.borderColor = "";
 };
 
 // По нажатию отправляю содержание строки:
-button.onclick = function() {
+button.onclick = function () {
 
-    // let i = 0;
-    // let leng = document.querySelectorAll('option').length;
-
-    // for (let i = 0; i < leng; i++) {
-    //     if (drop[i].value === drop.value) {
-    //         if (inp.value !== '') { 
-    //             drop[i].innerHTML = inp.value;
-    //         } else {
-    //             document.getElementById("inp").style.borderColor = "red";
-    //         } 
-    //     }
-    // }
-    
     let elems = document.querySelectorAll('option');
 
     elems.forEach(element => {
         if (element.value === drop.value) {
-            if (inp.value !== '') { 
+            if (inp.value !== '') {
                 element.innerHTML = inp.value;
             } else {
                 document.getElementById("inp").style.borderColor = "red";
-            } 
+            }
         }
     });
-
-  };
+};
